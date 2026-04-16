7d59abca0aff2322ad53923cce54ef2d9bfdfbfb
Alecsandru Soare <alecstwch@users.noreply.github.com>
2026-04-16 14:14:35 +0300

    gata funtie decision tree -btz

--- scripturi python/ml_algs/decision_tree.py
+++ scripturi python/ml_algs/decision_tree.py
@@ -9,20 +9,33 @@ decision_tree_parameters = {
 
 
 '''
-input data list sa contina obiecte de tipul:
+data list sa contina obiecte de tipul:
 {
     'name': -
-    'data': -
+    'Train_Predictors': -
+    'Train_Target': -
+    'Test_Predictors': -
+    'Test_Target': -
 }
 
 '''
 
-def grid_metrics_decision_tree(input_data_list: list, Y_train):
+def grid_metrics_decision_tree(data_list: list):
     rows = []
-    for dimension_reduction in input_data_list:
+    for dimension_reduction in data_list:
         for max_depth in decision_tree_parameters['max_depth']:
             for min_samples_split in decision_tree_parameters['min_samples_split']:
                 model = DecisionTreeRegressor(max_depth=max_depth, min_samples_split=min_samples_split)
+
+                model.fit(dimension_reduction['Train_Predictors'], dimension_reduction['Train_Target'])
+
+                predictions = model.predict(dimension_reduction['Test_Predictors'])
+
+                mse = mean_squared_error(dimension_reduction['Test_Target'], predictions)
+                mae = mean_absolute_error(dimension_reduction['Test_Target'], predictions)
+                f1 = f1_score(dimension_reduction['Test_Target'], predictions, average='weighted')
+                accuracy = accuracy_score(dimension_reduction['Test_Target'], predictions)
+
 
                 return_object = {
                     'dimension_reduction_type': dimension_reduction['name'],
@@ -30,11 +43,19 @@ def grid_metrics_decision_tree(input_data_list: list, Y_train):
                     'min_samples_split': min_samples_split,
                     'model': 'Decision Tree',
                     'scores': {
-                        'mean_squared_error': ,
-                        'mean_absolute_error': ,
-                        ''
+                        'mean_squared_error': mse,
+                        'mean_absolute_error': mae,
+                        'f1_score': f1,
+                        'accuracy_score': accuracy
                     }
                 }
+
+                rows.append(return_object)
+    return rows
+
+
+
+
 
 
 
